e2e5c2f1725069df655fd85c99a3b3d6ee92e5a6
Vitor Dino <vitor@kunstdesign.com.br>
2019-10-02 03:51:05 -0300

    :sparkles: create Text component

---
+++ src/components/Text/index.js
@@ -0,0 +1,22 @@
+import styled from 'styled-components'
+import { mapBreakpoints } from 'etymos'
+
+const getStyle = ({ fontSize, lineHeight, letterSpacing }) =>
+	[
+		fontSize && `font-size: ${fontSize}px`,
+		lineHeight && `line-height: ${lineHeight}px`,
+		letterSpacing && `letter-spacing: ${letterSpacing}`,
+	]
+		.filter(x => x)
+		.join(';\n')
+
+const Text = styled.p`
+  ${p => p.color && `color: ${p.theme.colors.light?.[p.color] || 'red'};`}
+  ${p => p.weight && `font-weight: ${p.weight};`}
+  ${p => p.uppercase && `text-transform: uppercase;`}
+	${mapBreakpoints((value, props) => getStyle(props.theme.type.scale[value]))}
+`
+
+Text.defaultProps = { xs: 'default' }
+
+export default Text
