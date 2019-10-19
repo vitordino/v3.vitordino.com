e781912e9df22b6f9843ab70f892d64d7346de58
Vitor Dino <vitor@kunstdesign.com.br>
2019-10-19 05:36:46 -0300

    :building_construction: container: use theme above function

--- src/components/Container/index.js
+++ src/components/Container/index.js
@@ -1,16 +1,15 @@
 import styled from 'styled-components'
-import { above } from 'etymos'
 
 const Container = styled.div`
 	padding-left: 1rem;
 	padding-right: 1rem;
 	max-width: 80rem;
 	margin: 0 auto;
-	${above('md')`
+	${p => p.theme.above('md')`
 		padding-left: 2rem;
 		padding-right: 2rem;
 	`}
-	${above('lg')`
+	${p => p.theme.above('lg')`
 		padding-left: 2.5rem;
 		padding-right: 2.5rem;
 	`}
