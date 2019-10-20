30044418f13821d8432c1b515b985507f72d85d9
Vitor Dino <vitor@kunstdesign.com.br>
2019-10-20 19:57:49 -0300

    :sparkles: mock home initial structure

--- src/indexes/home.js
+++ src/indexes/home.js
@@ -2,13 +2,21 @@ import React from 'react'
 import { graphql } from 'gatsby'
 
 import extractMeta from '@/utils/extractMeta'
+import useLocale from '@/store/locale'
 import Layout from '@/components/Layout'
+import Container from '@/components/Container'
+import NavBar from '@/components/NavBar'
 
-const HomePage = ({ data }) => {
+const HomePage = ({ data, ...props }) => {
+	const x = useLocale()
 	const content = data?.content?.data
+
 	return (
 		<Layout {...extractMeta(content)}>
-			<pre>{JSON.stringify(content, null, 2)}</pre>
+			<NavBar />
+			<Container>
+				<pre>{JSON.stringify({ x, props, content }, null, 2)}</pre>
+			</Container>
 		</Layout>
 	)
 }
