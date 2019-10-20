5cd7dfd9e7afdd46ea3639be73e5392052c0b935
Vitor Dino <vitor@kunstdesign.com.br>
2019-10-20 20:12:50 -0300

    :bug: fix Text GetTypeStyle function

--- src/components/Text/index.js
+++ src/components/Text/index.js
@@ -1,13 +1,13 @@
 import styled from 'styled-components'
 import { mapBreakpoints } from 'etymos'
 
-import getStyle from '@/utils/getTypeStyle'
+import getTypeStyle from '@/utils/getTypeStyle'
 
 const Text = styled.p`
   ${p => p.color && `color: ${p.theme.colors.light?.[p.color] || 'red'};`}
   ${p => p.weight && `font-weight: ${p.weight};`}
   ${p => p.uppercase && `text-transform: uppercase;`}
-	${mapBreakpoints((value, props) => getStyle(props.theme.type.scale[value]))}
+	${mapBreakpoints(value => getTypeStyle(value))}
 `
 
 Text.defaultProps = { xs: 0 }
