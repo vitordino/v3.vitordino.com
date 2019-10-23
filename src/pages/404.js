0fd33b85ea7f60698f08377ab767b2ad41809b91
Vitor Dino <vitor@kunstdesign.com.br>
2019-10-23 01:42:43 -0300

    :sparkles: mock 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,11 +1,17 @@
 import React from 'react'
-
 import Layout from '@/components/Layout'
+import NavBar from '@/components/NavBar'
+import Container from '@/components/Container'
+import Link from '@/components/Link'
 
 const NotFoundPage = () => (
 	<Layout title='404: Not found'>
-		<h1>NOT FOUND</h1>
-		<p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+		<NavBar />
+		<Container>
+			<h1>NOT FOUND</h1>
+			<p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+			<Link to='/'>home</Link>
+		</Container>
 	</Layout>
 )
 
