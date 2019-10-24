f9fdff71c6a5e9911bf7f76279b65db89ab198bb
Vitor Dino <vitor@kunstdesign.com.br>
2019-10-24 04:24:33 -0300

    :bug: fix get color function on Text component

--- src/components/Text/index.js
+++ src/components/Text/index.js
@@ -4,7 +4,7 @@ import { mapBreakpoints } from 'etymos'
 import getTypeStyle from '@/utils/getTypeStyle'
 
 const Text = styled.p`
-  ${p => p.color && `color: ${p.theme.colors.light?.[p.color] || 'red'};`}
+  ${p => p.color && `color: ${p.theme.colors.get(p.color)(p)};`}
   ${p => p.weight && `font-weight: ${p.weight};`}
   ${p => p.uppercase && `text-transform: uppercase;`}
 	${mapBreakpoints(value => getTypeStyle(value))}
