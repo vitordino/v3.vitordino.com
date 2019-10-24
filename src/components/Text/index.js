9993922df832bf743cada4ef55bd7602e5247088
Vitor Dino <vitor@kunstdesign.com.br>
2019-10-24 04:24:47 -0300

    :wrench: change default Text tag to div to prevent nesting

--- src/components/Text/index.js
+++ src/components/Text/index.js
@@ -3,7 +3,7 @@ import { mapBreakpoints } from 'etymos'
 
 import getTypeStyle from '@/utils/getTypeStyle'
 
-const Text = styled.p`
+const Text = styled.div`
   ${p => p.color && `color: ${p.theme.colors.get(p.color)(p)};`}
   ${p => p.weight && `font-weight: ${p.weight};`}
   ${p => p.uppercase && `text-transform: uppercase;`}
