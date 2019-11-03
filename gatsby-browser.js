1f4b822ef21a8274c7508c232532e8cab37a9fc3
Vitor Dino <vitor@kunstdesign.com.br>
2019-11-03 08:37:00 -0300

    :wrench: add intersection-observer polyfill

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,6 +1,12 @@
 import React from 'react'
 import GatsbyLayout from '@/components/Layout/Gatsby'
 
+export const onClientEntry = async () => {
+  if (typeof IntersectionObserver === `undefined`) {
+    await import(`intersection-observer`);
+  }
+}
+
 // eslint-disable-next-line import/prefer-default-export
 export const wrapPageElement = ({ element, props }) => (
 	<GatsbyLayout {...props}>{element}</GatsbyLayout>
