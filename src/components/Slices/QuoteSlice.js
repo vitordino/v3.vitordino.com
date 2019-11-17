3d1151b7d659730045caa119e62c488d322a7a6c
Vitor Dino <vitor@kunstdesign.com.br>
2019-11-17 03:19:23 -0300

    :sparkles: add decoration to quote slice

--- src/components/Slices/QuoteSlice.js
+++ src/components/Slices/QuoteSlice.js
@@ -19,22 +19,39 @@ const Content = styled(Text)`
 	}
 `
 
+const QuoteSymbolDecoration = styled.div`
+	font-size: 4rem;
+	line-height: 1rem;
+	color: ${p => getColor('base11')};
+	${p => p.theme.above('lg')`
+		text-align: right;
+		font-size: 6rem;
+		line-height: 4rem
+	`}
+	font-weight: 500;
+	&:after {
+		content: '“';
+	}
+`
+
 const QuoteSlice = ({ color, background, children, author }) => (
 	<Wrapper as='section' color={color} background={background}>
 		<Container>
 			<Spacer.V xs={8} />
 			<Grid.Row>
-				<Grid.Column xs={0} lg={1} xg={2} />
+				<Grid.Column xs={16} lg={1} xg={2}>
+					<QuoteSymbolDecoration />
+				</Grid.Column>
 				<Grid.Column xs={16} sm={14} md={12}>
 					{children && (
-						<Content xs={2} sm={5} lg={6} weight={500}>
+						<Content as='blockquote' xs={2} sm={5} lg={6} weight={500}>
 							{children}
 						</Content>
 					)}
 					{author && (
 						<>
 							<Spacer.V xs={2} />
-							<Text xs={0} color='base44'>
+							<Text as='cite' xs={0} color='base44'>
 								{author}
 							</Text>
 						</>
