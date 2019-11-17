1cf748978e621a755b822dd901350647a3f2837f
Vitor Dino <vitor@kunstdesign.com.br>
2019-11-17 04:03:08 -0300

    :sparkles: overwrite anchor element on mdx

--- src/templates/post.js
+++ src/templates/post.js
@@ -1,18 +1,23 @@
 import React from 'react'
 import { graphql } from 'gatsby'
+import { MDXProvider } from '@mdx-js/react'
 import { MDXRenderer } from 'gatsby-plugin-mdx'
 import Layout from '@/components/Layout'
 import NavBar from '@/components/NavBar'
 import Link from '@/components/Link'
 import PostHero from '@/components/PostHero'
 
-const components = { a: Link }
+const MDXLink = ({ href, ...props }) => <Link {...props} to={href} />
+
+const components = { a: MDXLink }
 
 const Post = ({ data: { mdx } }) => (
 	<Layout {...mdx.frontmatter}>
 		<NavBar />
 		<PostHero {...mdx.frontmatter} {...mdx.frontmatter.hero} />
-		<MDXRenderer components={components}>{mdx.body}</MDXRenderer>
+		<MDXProvider components={components}>
+			<MDXRenderer>{mdx.body}</MDXRenderer>
+		</MDXProvider>
 	</Layout>
 )
 
