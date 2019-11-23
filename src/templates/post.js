026776baa0a78db4d20cb8ca9f1e15133a34cf4a
Vitor Dino <vitor@kunstdesign.com.br>
2019-11-23 18:20:04 -0300

    :sparkles: render h2 on post

--- src/templates/post.js
+++ src/templates/post.js
@@ -2,17 +2,21 @@ import React from 'react'
 import { graphql } from 'gatsby'
 import { MDXProvider } from '@mdx-js/react'
 import { MDXRenderer } from 'gatsby-plugin-mdx'
+
 import Layout from '@/components/Layout'
 import NavBar from '@/components/NavBar'
+import Text from '@/components/Text'
 import Link from '@/components/Link'
 import PostHero from '@/components/PostHero'
 import CodeBlock from '@/components/CodeBlock'
 
 const MDXLink = ({ href, ...props }) => <Link {...props} to={href} />
+const H2 = p => <Text xs={3} md={5} {...p} />
 
 const components = {
 	a: MDXLink,
 	pre: CodeBlock,
+	h2: H2,
 }
 
 const Post = ({ data: { mdx } }) => (
