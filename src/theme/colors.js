bcd12f3e8938b0702c38d18681c6112dad1bfe5e
Vitor Dino <vitor@kunstdesign.com.br>
2019-11-23 18:20:20 -0300

    :recycle: theme colors

--- src/theme/colors.js
+++ src/theme/colors.js
@@ -12,30 +12,30 @@ const get = color => ({ theme, ...p }) => {
 	return theme?.colors?.[theme.colors.mode]?.[color]
 }
 
-const colors = {
-	get,
-	light: {
-		base: base.light,
-		base88: mix(0.88, base.light, base.dark),
-		base66: mix(0.66, base.light, base.dark),
-		base44: mix(0.44, base.light, base.dark),
-		base22: mix(0.22, base.light, base.dark),
-		base11: mix(0.11, base.light, base.dark),
-		base06: mix(0.06, base.light, base.dark),
-		base03: mix(0.03, base.light, base.dark),
-		base00: mix(0.0, base.light, base.dark),
-	},
-	dark: {
-		base: base.dark,
-		base88: mix(0.88, base.dark, base.light),
-		base66: mix(0.66, base.dark, base.light),
-		base44: mix(0.44, base.dark, base.light),
-		base22: mix(0.22, base.dark, base.light),
-		base11: mix(0.11, base.dark, base.light),
-		base06: mix(0.06, base.dark, base.light),
-		base03: mix(0.03, base.dark, base.light),
-		base00: mix(0.0, base.dark, base.light),
-	},
+const light = {
+	base: base.light,
+	base88: mix(0.88, base.light, base.dark),
+	base66: mix(0.66, base.light, base.dark),
+	base44: mix(0.44, base.light, base.dark),
+	base22: mix(0.22, base.light, base.dark),
+	base11: mix(0.11, base.light, base.dark),
+	base06: mix(0.06, base.light, base.dark),
+	base03: mix(0.03, base.light, base.dark),
+	base00: mix(0.0, base.light, base.dark),
 }
+
+const dark = {
+	base: base.dark,
+	base88: mix(0.88, base.dark, base.light),
+	base66: mix(0.66, base.dark, base.light),
+	base44: mix(0.44, base.dark, base.light),
+	base22: mix(0.22, base.dark, base.light),
+	base11: mix(0.11, base.dark, base.light),
+	base06: mix(0.06, base.dark, base.light),
+	base03: mix(0.03, base.dark, base.light),
+	base00: mix(0.0, base.dark, base.light),
+}
+
+const colors = { get, light, dark }
 
 export default colors
