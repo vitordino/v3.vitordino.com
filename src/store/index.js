6e10ed9178a38589763e113b34b0139c9c1aaf4f
leonardodino <leonardo@kunstdesign.com.br>
2020-02-14 23:11:29 +0000

    :poop: reload react tree on browser to fix theme

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,22 @@
-import React, { createContext } from 'react'
+import React, {
+	createContext,
+	useMemo,
+	useReducer,
+	useLayoutEffect,
+} from 'react'
 import locales from '../../content/i18n'
 
 const Context = createContext()
 
 export const useContext = () => React.useContext(Context)
 
-export const AppProvider = ({ locale, children }) => (
-	<Context.Provider value={{ locales, locale }}>{children}</Context.Provider>
-)
+export const AppProvider = ({ locale, children }) => {
+	const value = useMemo(() => ({ locales, locale }), [locale])
+	const [key, dispatch] = useReducer(a => !a)
+	useLayoutEffect(dispatch, [])
+	return (
+		<Context.Provider key={key} value={value}>
+			{children}
+		</Context.Provider>
+	)
+}
