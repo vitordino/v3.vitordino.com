9e617deefc2a45fa34fb690b2b8c3851cadb0132
Vitor Dino <vitor@kunstdesign.com.br>
2020-03-02 23:12:12 -0300

    :fire: remove state from theme

--- src/theme/index.js
+++ src/theme/index.js
@@ -2,41 +2,21 @@ import React from 'react'
 import { ThemeProvider as Provider } from 'styled-components'
 import { above } from 'etymos'
 
-import usePersistentState from '@/hooks/usePersistentState'
-
 import colors from './colors'
 import responsive from './responsive'
 import typography from './type'
 import transition from './transition'
 
-const theme = { colors, ...responsive, type: typography, transition }
-
-const keys = {
-	color: 'theme:color-mode',
-	grid: 'theme:show-grid',
+const theme = {
+	above,
+	colors,
+	...responsive,
+	type: typography,
+	transition,
 }
 
-export const ThemeProvider = ({ children }) => {
-	const [colorMode, setColorMode] = usePersistentState(keys.color, 'light')
-	const [isGridVisible, setGridVisible] = usePersistentState(keys.grid, false)
-
-	return (
-		<Provider
-			theme={{
-				above,
-				colors,
-				colorMode,
-				setColorMode,
-				...responsive,
-				isGridVisible,
-				setGridVisible,
-				type: typography,
-				transition,
-			}}
-		>
-			{children}
-		</Provider>
-	)
-}
+export const ThemeProvider = ({ children }) => (
+	<Provider theme={theme}>{children}</Provider>
+)
 
 export default theme
