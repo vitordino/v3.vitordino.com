9dddc5df0fa07503a5294e86af1902c041011e60
Vitor Dino <vitor@kunstdesign.com.br>
2020-03-28 12:24:15 -0300

    :sparkles: add ImageSlice

---
+++ src/components/Slices/ImageSlice.js
@@ -0,0 +1,49 @@
+import React from 'react'
+import styled, { css } from 'styled-components'
+
+import Spacer from '@/components/Spacer'
+import Text from '@/components/Text'
+import Container from '@/components/Container'
+import ColorMode from '@/components/ColorMode'
+
+const Wrapper = styled.div`
+	${({ color, background, theme }) => css`
+		color: var(--color-${color});
+		background: var(--color-${background});
+		${theme.transition.get()};
+	`}
+`
+
+const Caption = styled(Text).attrs({
+	as: 'figcaption',
+	background: 'base03',
+	color: 'base66',
+})`
+	display: block;
+	padding: 1.5rem 0;
+	max-width: 2400px;
+	margin: auto;
+	a {
+		text-decoration: underline;
+	}
+`
+
+const ImageSlice = ({ colorMode, children, caption }) => (
+	<ColorMode mode={colorMode}>
+		<Wrapper as='figure'>
+			{children}
+			{caption && (
+				<Caption>
+					<Container>{caption}</Container>
+				</Caption>
+			)}
+		</Wrapper>
+	</ColorMode>
+)
+
+ImageSlice.defaultProps = {
+	color: 'base',
+	background: 'base00',
+}
+
+export default ImageSlice
--- src/components/Slices/index.js
+++ src/components/Slices/index.js
@@ -1,9 +1,11 @@
 import { default as Text } from './TextSlice'
 import { default as Quote } from './QuoteSlice'
+import { default as Image } from './ImageSlice'
 
 const Slices = {
 	Text,
 	Quote,
+	Image,
 }
 
 export default Slices
