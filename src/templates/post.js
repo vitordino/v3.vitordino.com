308a1d01a29699102eb9a485223d6776f91de498
Vitor Dino <vitor@kunstdesign.com.br>
2020-04-03 04:30:16 -0300

    :truck: rename Link to Clickable

--- src/templates/post.js
+++ src/templates/post.js
@@ -6,11 +6,11 @@ import { MDXRenderer } from 'gatsby-plugin-mdx'
 import Layout from '~/components/Layout'
 import NavBar from '~/components/NavBar'
 import Text from '~/components/Text'
-import Link from '~/components/Link'
+import Clickable from '~/components/Clickable'
 import PostHero from '~/components/PostHero'
 import CodeBlock from '~/components/CodeBlock'
 
-const MDXLink = ({ href, ...props }) => <Link {...props} to={href} />
+const MDXLink = ({ href, ...props }) => <Clickable {...props} to={href} />
 const H2 = p => <Text xs={3} md={5} {...p} />
 
 const components = {
