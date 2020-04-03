914b6797ebe613f33f5afbc788dc358ac0b9a127
Vitor Dino <vitor@kunstdesign.com.br>
2020-04-03 07:49:10 -0300

    :sparkles: utterances change color mode

--- src/components/Utterances/index.js
+++ src/components/Utterances/index.js
@@ -1,20 +1,55 @@
-import React from 'react'
+import React, {
+	useRef,
+	useLayoutEffect,
+	useEffect,
+	memo,
+	useCallback,
+} from 'react'
 
-const Utterances = ({ uid }) => (
-	<section
-		ref={ref => {
-			if (!ref) return
-			const el = document.createElement('script')
-			el.src = 'https://utteranc.es/client.js'
-			el.async = true
-			el.crossOrigin = 'anonymous'
-			el.setAttribute('repo', 'vitordino/vitordino.com')
-			el.setAttribute('issue-term', uid)
-			el.setAttribute('label', 'comments 💬')
-			el.setAttribute('theme', 'github-light')
-			ref.appendChild(el)
-		}}
-	/>
-)
+import { useColorMode, useGridMode } from '~/store'
 
-export default Utterances
+const removeChilds = node => {
+	let last
+	while ((last = node.lastChild)) node.removeChild(last)
+}
+
+const Utterances = memo(({ uid, theme }) => {
+	const ref = useRef(null)
+
+	/* eslint-disable */
+	useLayoutEffect(() => {
+		if (!ref.current) return
+		removeChilds(ref.current)
+		const el = document.createElement('script')
+		el.src = 'https://utteranc.es/client.js'
+		el.async = true
+		el.crossOrigin = 'anonymous'
+		el.setAttribute('repo', 'vitordino/vitordino.com')
+		el.setAttribute('issue-term', uid)
+		el.setAttribute('label', 'comments 💬')
+		el.setAttribute('theme', theme)
+		ref.current.appendChild(el)
+	}, [uid])
+	/* eslint-enable */
+
+	const message = { type: 'set-theme', theme }
+
+	/* eslint-disable */
+	useEffect(() => {
+		const iframe = document.querySelector('.utterances-frame')
+		if (iframe) {
+			iframe.contentWindow.postMessage(message, 'https://utteranc.es')
+		}
+	}, [theme, uid])
+	/* eslint-enable */
+
+	return <section ref={ref} />
+})
+
+const withColorMode = Component => props => {
+	const colorMode = useColorMode()
+	const theme = colorMode.includes('dark') ? 'photon-dark' : 'github-light'
+	return <Component theme={theme} {...props} />
+}
+
+export default withColorMode(Utterances)
