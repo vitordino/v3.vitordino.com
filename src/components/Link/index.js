3f7318aeae4598e392eaa3621ef8d0ab914e70f2
Vitor Dino <vitor@kunstdesign.com.br>
2020-04-03 00:05:11 -0300

    :sparkles: treat query params routes on Link

--- src/components/Link/index.js
+++ src/components/Link/index.js
@@ -1,23 +1,43 @@
 import React from 'react'
 import { Link as GatsbyLink } from 'gatsby'
+import { navigate } from '@reach/router'
 import useLocale from '~/store/locale'
 
 /* eslint-disable jsx-a11y/anchor-has-content */
 /* eslint-disable react/button-has-type */
+
+const onSearchClick = url => e => {
+	e.preventDefault()
+	return navigate(url)
+}
+
+const getLocalizedPath = ({ locales, locale, href }) => {
+	const url = href.replace(/^\/|\/$/g, '')
+	if (url.startsWith('?')) return url
+	const isIndex = href === '/'
+	return !locales[locale] || locales[locale].default
+		? url
+		: `${locales[locale].path}${isIndex ? '' : `${url}`}`
+}
+
 const Link = ({ to, children, className, style, target, ...p }) => {
 	const { locales, locale } = useLocale()
 
 	const href = (to || {}).pathname || typeof to === 'string' ? to : null
 	const props = { className, style, children, target }
 
-	const isIndex = href === '/'
-	const localized =
-		!locales[locale] || locales[locale].default
-			? href
-			: `${locales[locale].path}${isIndex ? '' : `${href}`}`
+	const localized = getLocalizedPath({ locales, locale, href })
 
 	if (p.onClick || p.type)
 		return <button type={p.type || 'button'} {...p} {...props} />
+	if (/^[.?]/.test(href))
+		return (
+			<GatsbyLink
+				{...props}
+				href={localized}
+				onClick={onSearchClick(localized)}
+			/>
+		)
 	if (/^[./]/.test(href)) return <GatsbyLink {...props} to={localized} />
 	if (/^[.#]/.test(href)) return <a {...p} {...props} href={href} />
 	if (/^[.http]/.test(href))
