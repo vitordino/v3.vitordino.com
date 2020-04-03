5f30e8895adeb6cabfda301aac9a622500eeaada
Vitor Dino <vitor@kunstdesign.com.br>
2020-04-03 04:27:37 -0300

    :sparkles: add netlify + inputs on ContactModal

--- src/components/ContactModal/index.js
+++ src/components/ContactModal/index.js
@@ -1,14 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled, { css } from 'styled-components'
 
+import useNetlifyForm, { STATUS } from '~/hooks/useNetlifyForm'
 import Portal from '~/components/Portal'
 import ScrollLock from '~/components/ScrollLock'
 import Container from '~/components/Container'
 import Grid from '~/components/Grid'
 import Spacer from '~/components/Spacer'
 import Text from '~/components/Text'
+import Input from '~/components/Input'
 
-const Wrapper = styled.div`
+const Wrapper = styled.form`
 	display: block;
 	position: fixed;
 	top: 0;
@@ -42,11 +44,19 @@ const Backdrop = styled.div`
 		`}
 `
 
+const Scroll = styled.div`
+	overflow-y: auto;
+	overflow-x: visible;
+	max-height: 100vh;
+	padding: 0px 60px 60px;
+	margin: -0px -60px -60px;
+`
+
 const Inner = styled.div`
 	position: relative;
 	background: var(--color-base00);
 	border-radius: 0 0 0.5rem 0.5rem;
-	transform: translateY(-150%);
+	transform: translateY(-100%);
 	transition: 0.25s transform 0;
 	${({ isOpen }) =>
 		isOpen &&
@@ -74,32 +84,75 @@ const Inner = styled.div`
 	}
 `
 
+const initialValues = {
+	name: '',
+	email: '',
+	message: '',
+}
+
 const ContactModal = ({ isOpen, onClose }) => {
+	const [state, setState] = useState(() => initialValues)
+	const [handleSubmit, status] = useNetlifyForm({ name: 'contact', state })
+
+	const handleChange = e =>
+		setState({ ...state, [e.target.name]: e.target.value })
+
+	const disabled = status === STATUS.LOADING || status === STATUS.SUCCESS
+
 	return (
 		<Portal>
 			{isOpen && <ScrollLock />}
-			<Wrapper isOpen={isOpen}>
+			<Wrapper name='contact' onSubmit={handleSubmit} isOpen={isOpen}>
 				<Backdrop isOpen={isOpen} onClick={onClose} />
 				<Container>
 					<Grid.Row style={{ justifyContent: 'center' }}>
 						<Grid.Column xs={16} sm={14} md={12} lg={10} xg={8}>
-							<Inner isOpen={isOpen}>
-								<Grid.Row style={{ justifyContent: 'center' }}>
-									<Grid.Column
-										xs={14}
-										sm={(12 * 16) / 14}
-										md={(10 * 16) / 12}
-										lg={(8 * 16) / 10}
-										xg={12}
-									>
-										<Spacer.V xs={1} sm={1.5} md={2} />
-										<Text xs={1} md={2} weight={500}>
-											Hey there 👋
-										</Text>
-										<Spacer.V xs={2} />
-									</Grid.Column>
-								</Grid.Row>
-							</Inner>
+							<Scroll>
+								<Inner isOpen={isOpen}>
+									<Grid.Row style={{ justifyContent: 'center' }}>
+										<Grid.Column
+											xs={14}
+											sm={(12 * 16) / 14}
+											md={(10 * 16) / 12}
+											lg={(8 * 16) / 10}
+											xg={12}
+										>
+											<Spacer.V xs={1} sm={1.5} md={2} />
+											<Text xs={1} md={2} weight={500}>
+												Hey there 👋
+											</Text>
+											<Input
+												name='name'
+												value={state.name}
+												onChange={handleChange}
+												disabled={disabled}
+											/>
+											<Input
+												name='email'
+												type='email'
+												value={state.email}
+												onChange={handleChange}
+												disabled={disabled}
+												required
+											/>
+											<Input
+												textarea
+												name='message'
+												type='message'
+												value={state.message}
+												onChange={handleChange}
+												disabled={disabled}
+												required
+											/>
+											<button type='button' onClick={onClose}>
+												close
+											</button>
+											<button type='submit'>submit</button>
+											<Spacer.V xs={2} />
+										</Grid.Column>
+									</Grid.Row>
+								</Inner>
+							</Scroll>
 						</Grid.Column>
 					</Grid.Row>
 				</Container>
