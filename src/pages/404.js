fe55c1095435aa6f4a5de47b62b72e32d7fd3180
Vitor Dino <vitor@kunstdesign.com.br>
2020-04-03 00:57:11 -0300

    :sparkles: custom 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,16 +2,25 @@ import React from 'react'
 import Layout from '~/components/Layout'
 import NavBar from '~/components/NavBar'
 import Container from '~/components/Container'
-import Link from '~/components/Link'
+import Text from '~/components/Text'
 
 const NotFoundPage = () => (
 	<Layout title='404: Not found'>
 		<NavBar />
-		<Container>
-			<h1>NOT FOUND</h1>
-			<p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-			<Link to='/'>home</Link>
-		</Container>
+		<Text
+			as='h1'
+			style={{
+				fontSize: '50vw',
+				textAlign: 'center',
+				lineHeight: 1,
+				letterSpacing: '-1vw',
+			}}
+			color='base06'
+			weight={300}
+			family='sans'
+		>
+			404
+		</Text>
 	</Layout>
 )
 
